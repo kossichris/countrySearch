8822151292617a8b405817155bf476c45bd2551b
Chrissmal <chrishouns21@gmail.com>
2021-06-09 07:49:01 -0700

    search corrected

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -21,14 +21,8 @@ $(document).ready(function () {
                         collection.push(item);
                     }
                 });
-                Africa = map.get("Africa")
-                Asia = map.get("Asia")
-                Europe = map.get("Europe")
-                Americas = map.get("Americas")
-                Oceanie = map.get("Oceania")
-                console.log(map, Oceanie)
+                Africa = map.get("Africa");  Asia = map.get("Asia");  Europe = map.get("Europe"); Americas = map.get("Americas");  Oceanie = map.get("Oceania")
 
-                // $('#nights').html();
                 $("#region1").html('Africa');
                 $.each(Africa, function (index, value) {
                     $("#c1").append('<img class="flag" src="' + value.flag + '" />' + '  ' + value.name + ' <br/> ');
@@ -73,63 +67,47 @@ $("#myInput").on("keyup", function () {
     listCountries.forEach((el) => {
         el.name.toLowerCase().trim().indexOf(value) !== -1 ? filteredCountries.push({ region: el.region, name: el.name, flag: el.flag }) : null
     });
-    console.log(value)
-
     if (filteredCountries.length > 0) {
         $('#notFound').hide();
+        $('#region1').hide(); $('#region2').hide(); $('#region3').hide(); $('#region4').hide(); $('#region5').hide();
+        $('#c1').hide(); $('#c2').hide(); $('#c3').hide(); $('#c4').hide(); $('#c5').hide();
 
-        $('#region1').hide();
-        $('#region2').hide();
-        $('#region3').hide();
-        $('#region4').hide();
-        $('#region5').hide();
+        var africa = filteredCountries.filter(element => { return element.region === "Africa" });
+        var asia = filteredCountries.filter(element => { return element.region === "Asia" });
+        var europe = filteredCountries.filter(element => { return element.region === "Europe" });
+        var americas = filteredCountries.filter(element => { return element.region === "Americas" });
+        var oceania = filteredCountries.filter(element => { return element.region === "Oceania" });
 
-        $('#c1').hide();
-        $('#c2').hide();
-        $('#c3').hide();
-        $('#c4').hide();
-        $('#c5').hide();
+        $("#c1").empty(); $("#c2").empty(); $("#c3").empty(); $("#c4").empty(); $("#c5").empty();
 
-        $.each(filteredCountries, function (index, value) {
-            $("#c").empty();
-            $("#region").html(value.region);
-            $("#c").append('<img class="flag" src="' + value.flag + '" />' + '  ' + value.name + ' <br/> ');
-        });
-        console.log($("#c"))
-        $('#region').show();
-        $('#c').show();
-        filteredCountries = []
+
+        africa.length > 0 ? $("#region1").html('Africa'): null;
+        $.each(africa, function (index, value) { $("#c1").append('<img class="flag" src="' + value.flag + '" />' + '  ' + value.name + ' <br/> '); });
+
+        europe.length > 0 ? $("#region2").html('Europe') :null;
+        $.each(europe, function (index, value) { $("#c2").append('<img class="flag" src="' + value.flag + '" />' + '  ' + value.name + ' <br/> '); });
+
+        asia.length > 0 ? $("#region3").html('Asia'): null;
+        $.each(asia, function (index, value) { $("#c3").append('<img class="flag" src="' + value.flag + '" />' + '  ' + value.name + ' <br/> '); });
+
+        americas.length > 0 ? $("#region4").html('America'): null;
+        $.each(americas, function (index, value) { $("#c4").append('<img class="flag" src="' + value.flag + '" />' + '  ' + value.name + ' <br/> '); });
+
+        oceania.length > 0 ? $("#region5").html('Oceania'): null;
+        $.each(oceania, function (index, value) {$("#c5").append('<img class="flag" src="' + value.flag + '" />' + '  ' + value.name + ' <br/> ');});
+
+        $('#region1').show();$('#region2').show();$('#region3').show();$('#region4').show();$('#region5').show();
+        $('#c1').show();$('#c2').show();$('#c3').show();$('#c4').show();$('#c5').show();
+
+        africa = []; europe = []; americas = []; oceania = [] ;asia = []
+
 
     } else if (filteredCountries.length === 0 && value.length > 0) {
-        $('#notFound').show();
-        $('#region1').hide();
-        $('#region2').hide();
-        $('#region3').hide();
-        $('#region4').hide();
-        $('#region5').hide();
-
-        $('#c1').hide();
-        $('#c2').hide();
-        $('#c3').hide();
-        $('#c4').hide();
-        $('#c5').hide();
-
-
+        $('#notFound').show();$('#region1').hide();$('#region2').hide();$('#region3').hide();$('#region4').hide();$('#region5').hide();
+        $('#c1').hide();$('#c2').hide();$('#c3').hide();$('#c4').hide();$('#c5').hide();
     } else if (value.length === 0) {
         console.log("value is empty")
-        $('#notFound').hide();
-        $('#region').hide();
-        $('#c').hide();
-        $("#region1").show();
-        $("#c1").show();
-        $("#region2").show();
-        $("#c2").show();
-        $("#region3").show();
-        $("#c3").show();
-        $("#region4").show();
-        $("#c4").show();
-        $("#region5").show();
-        $("#c5").show();
-
+        $('#notFound').hide();  $("#region1").show();$("#c1").show(); $("#region2").show();$("#c2").show();$("#region3").show();
+        $("#c3").show();$("#region4").show();$("#c4").show();$("#region5").show();$("#c5").show();
     }
 });
